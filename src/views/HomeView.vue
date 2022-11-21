237291b5d6b873b58b2cf3f1c8f813725ba7b878
B B <std.62122420108@ubru.ac.th>
2022-11-21 23:13:11 +0700

    0.3

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,45 @@
-<template>hello world</template>
+<template>
+  <v-card>
+    <v-layout>
+      <v-navigation-drawer v-model="drawer" temporary>
+        <v-list-item title="John Leider"></v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list nav>
+          <v-list-item
+            prepend-icon="mdi-home"
+            style="text"
+            title="Home"
+            value="home"
+          ></v-list-item>
+          <v-list-item
+            prepend-icon="mdi-home"
+            title="About"
+            value="about"
+          ></v-list-item>
+          <v-list-item
+            prepend-icon="mdi-chat"
+            title="About"
+            value="about"
+          ></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <v-main style="height: 100vh">
+        <div class="d-flex justify-center align-center h-100">
+          <v-btn color="primary" @click.stop="drawer = !drawer"> Toggle </v-btn>
+        </div>
+      </v-main>
+    </v-layout>
+  </v-card>
+</template>
 
 <script>
-import { defineComponent } from "vue";
-import { db } from "../DB";
-import { doc, setDoc } from "firebase/firestore";
-
-//  Components
-
-await setDoc(doc(db, "cities", "LA"), {
-  name: "Los Angeles",
-  state: "CA",
-  country: "USA",
-});
-
-export default defineComponent({
-  name: "HomeView",
-
-  components: {},
-});
+export default {
+  data() {
+    return {
+      drawer: null,
+    };
+  },
+};
 </script>
